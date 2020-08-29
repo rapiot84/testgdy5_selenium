250a49ebb7d8e8fbb161411737b1f807937fe423
username <rapiot@wp.pl>
2020-08-29 14:50:51 +0200

    implemented podcast test

--- src/test/java/FirstTests.java
+++ src/test/java/FirstTests.java
@@ -5,6 +5,13 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
 
 public class FirstTests {
 
@@ -20,18 +27,44 @@ public class FirstTests {
         driver=new ChromeDriver(); //przypisanie do pustej przegladarki, przegladarki chrome
         driver.manage().window().maximize();
         driver.get("https://dev.to");
+       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //przed kazdym kolejnym findElements poczekaj 10 s zanim wywalisz error, co sekunde sprawdzaj czy element jest juz dostepny
+
 
     }
 
     @Test
-    public void firstTest(){
-driver.get("https://dev.to");
+    public void goToPodcastsAndSelectThirdOneAndPlayIt(){
         WebElement podcastBtn = driver.findElement(By.xpath("//a[@href='/pod']"));
         highlightElement(driver,podcastBtn);
+        podcastBtn.click();
+        String podcastUrl = driver.getCurrentUrl();
+        //przejscie na strone z podcastami
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.urlToBe("https://dev.to/pod"));
+
+        List<WebElement> podcasts = driver.findElements(By.tagName("h3"));
+       // for (WebElement podcast: podcasts){
+      //      highlightElement(driver,podcast);
+      //  }
+        WebElement thirdPodcast = podcasts.get(2);
+        thirdPodcast.click();
+        WebElement recordBtn = driver.findElement(By.className("record-wrapper"));
+        recordBtn.click();
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.className("status-message"))));
+       String recordBtnClassAttribute = recordBtn.getAttribute("class");
+       boolean isPlaying = recordBtnClassAttribute.contains("playing");
+       assertTrue( "podcast wasn't played", recordBtnClassAttribute.contains("playing"));
+
     }
 
-    @Test
-    public void secondTest(){
-    }
 
-}
+        @Test public void secondTest(){
+            WebElement weekBtn = driver.findElement(By.xpath("//*[@id=\"on-page-nav-controls\"]/div/nav/a[2]"));
+        weekBtn.click(); WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.urlToBe("dev.to/top/week"));
+        WebElement firstArticle= driver.findElement(By.xpath("//*[@id=\"article-link-434803\"]"));
+        firstArticle.click();
+    }}
+
+
